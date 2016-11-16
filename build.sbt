362dcc0e7a1530cdf09fe78ee466434e278fe2a5
Chidambara Nadig <chidambaranadig@gmail.com>
2016-11-15 23:20:51 -0800

    More updates

--- build.sbt
+++ build.sbt
@@ -4,6 +4,14 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.0"
-
 libraryDependencies += "org.scala-lang" % "scala-compiler" % "2.11.8"
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.4"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.0.0"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql_2.11
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.0.0"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-tags_2.11
+libraryDependencies += "org.apache.spark" % "spark-tags_2.11" % "2.0.0"
--- src/main/scala/Survey.scala
+++ src/main/scala/Survey.scala
@@ -1,29 +1,84 @@
 package com.cnadig.youth.tobacco.survey
+
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions._
+
+import org.apache.spark.sql.functions._
+
 object Survey{
 
   def main(args : Array[String]) = {
-    println("HelloWorld")
+
 
     val spark = SparkSession
-      .builder()
-      .appName("Spark SQL basic example")
-      .getOrCreate()
+                  .builder()
+                  .appName("Spark SQL basic example")
+                  .enableHiveSupport()
+                  .getOrCreate()
 
+    import spark.implicits._
 
     val tobacco_schema_path = "/home/cnadig/Developer/CodingExercise/tobacco_schema.json"
-    val tobaccoFieldsArray = spark.read.json(tobacco_schema_path).select(explode($"meta.view.columns").as("columns")).select("columns.name").map(x => x.getString(0)).collect()
+    val tobaccoSchema = spark.read.json(tobacco_schema_path)
+
+    val tobaccoDataColumns = tobaccoSchema.select($"meta.view.columns.name").first
+                                          .mkString(",")
+                                          .replaceFirst("WrappedArray\\(","")
+                                          .replaceAll("\\)","")
+                                          .replaceAll("\\s","")
+                                          .split(",")
 
 
     val tobacco_data_path = "/home/cnadig/Developer/CodingExercise/tobacco_data.json"
     val tobaccoData = spark.read.json(tobacco_data_path).select("data")
-    val elements = tobaccoData.select($"data").first.getList(0).size
-    val tobaccoDataDF = tobaccoData.select((Range(0, elements).map(idx => $"data"(idx) as tobaccoFieldsArray(idx) ):_*))
+    val tobaccoDataDF = tobaccoData.select(
+            $"data"(0) as "sid",
+            $"data"(1) as "id",
+            $"data"(2) as "position",
+            $"data"(3) as "created_at",
+            $"data"(4) as "created_meta",
+            $"data"(5) as "updated_at",
+            $"data"(6) as "updated_meta",
+            $"data"(7) as "meta",
+            $"data"(8) as "YEAR",
+            $"data"(9) as "LocationAbbr",
+            $"data"(10) as "LocationDesc",
+            $"data"(11) as "TopicType",
+            $"data"(12) as "TopicDesc",
+            $"data"(13) as "MeasureDesc",
+            $"data"(14) as "DataSource",
+            $"data"(15) as "Response",
+            $"data"(16) as "Data_Value_Unit",
+            $"data"(17) as "Data_Value_Type",
+            $"data"(18) as "Data_Value",
+            $"data"(19) as "Data_Value_Footnote_Symbol",
+            $"data"(20) as "Data_Value_Footnote",
+            $"data"(21) as "Data_Value_Std_Err",
+            $"data"(22) as "Low_Confidence_Limit",
+            $"data"(23) as "High_Confidence_Limit",
+            $"data"(24) as "Sample_Size",
+            $"data"(25) as "Gender",
+            $"data"(26) as "Race",
+            $"data"(27) as "Age",
+            $"data"(28) as "Education",
+            $"data"(29) as "GeoLocation",
+            $"data"(30) as "TopicTypeId",
+            $"data"(31) as "TopicId",
+            $"data"(32) as "MeasureId",
+            $"data"(33) as "StratificationID1",
+            $"data"(34) as "StratificationID2",
+            $"data"(35) as "StratificationID3",
+            $"data"(36) as "StratificationID4",
+            $"data"(37) as "SubMeasureID",
+            $"data"(38) as "DisplayOrder"
+    )
 
+    val topicDimension = tobaccoDataDF.select($"TopicType",$"TopicDesc").distinct.filter(x => x.getString(0) != null)
+    val topicDimensionWithId = topicDimension.select( concat($"TopicType", lit(","), $"TopicDesc") as "Topic Dimension").withColumn("TopicId",monotonically_increasing_id)
 
-
-
+    topicDimensionWithId.write.mode("verwrite").save("/home/cnadig/Developer/CodingExercise/TopicDimension.parquet")ppppppppppp47
+    \\\
 
   }
-
 }
